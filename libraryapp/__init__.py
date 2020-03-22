a9c7fccea8dfaa188ad7bbe5e9faaf8c96a11edc
dianarand <diana.rand@outlook.com>
2020-03-22 23:48:37 +0200

    additional small changes

--- libraryapp/__init__.py
+++ libraryapp/__init__.py
@@ -3,19 +3,25 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from libraryapp.config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
+db = SQLAlchemy()
 
-db = SQLAlchemy(app)
-
-login_manager = LoginManager(app)
+login_manager = LoginManager()
 login_manager.login_view = 'main.login'
 login_manager.login_message = 'Logi sisse, et näha seda lehte'
 
-from libraryapp.main.routes import main
-from libraryapp.books.routes import books
-from libraryapp.lenders.routes import lenders
 
-app.register_blueprint(main)
-app.register_blueprint(books)
-app.register_blueprint(lenders)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from libraryapp.main.routes import main
+    from libraryapp.books.routes import books
+    from libraryapp.lenders.routes import lenders
+    app.register_blueprint(main)
+    app.register_blueprint(books)
+    app.register_blueprint(lenders)
+
+    return app
