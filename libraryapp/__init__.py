47b15b8bd163e330facd677c63ae5fb00b86d425
dianarand <diana.rand@outlook.com>
2020-03-24 12:27:29 +0200

    clean up

--- libraryapp/__init__.py
+++ libraryapp/__init__.py
@@ -1,17 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from libraryapp.config import Config
-import logging
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
-
-# logging.basicConfig(
-#     filename='appl.log',
-#     level=logging.INFO,
-#     format='%(asctime)s : %(name)s : %(levelname)s : %(message)s'
-# )
 
 from libraryapp import routes
